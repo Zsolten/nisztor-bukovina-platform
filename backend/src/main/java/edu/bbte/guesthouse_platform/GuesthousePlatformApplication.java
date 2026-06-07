38c275524cb6adc9ebdc12c29881e6739a68ecdd
Zsolten <nistorzsolt5@gmail.com>
2026-06-07 18:36:35 +0300

    Initial frontend and backend skeleton

---
+++ backend/src/main/java/edu/bbte/guesthouse_platform/GuesthousePlatformApplication.java
@@ -0,0 +1,13 @@
+package edu.bbte.guesthouse_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuesthousePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuesthousePlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/bbte/guesthouse_platform/GuesthousePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bbte.guesthouse_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuesthousePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
